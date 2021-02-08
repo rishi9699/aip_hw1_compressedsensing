32d14385cda9b468c07f631fb1dfc034308c66f0
rishi9699 <rishi9699@gmail.com>
2021-02-08 15:54:32 +0530

    Patch-wise A matrix instead of entire A

--- testscript.m
+++ testscript.m
@@ -1,9 +1,9 @@
-rows=288;
-columns=352;
+num_rows=288;
+num_columns=352;
 num_frames=3;
 
 video=mmread('../HW1/cars.avi');
-frames = uint8(zeros(rows, columns, num_frames));
+frames = uint8(zeros(num_rows, num_columns, num_frames));
 for i=1:num_frames
     frames(:,:,i) = im2gray(video.frames(i).cdata);
 end
@@ -15,7 +15,7 @@ imshow(frames(:,:,2))
 figure
 imshow(frames(:,:,3))
 
-random_pattern = uint8(binornd(1,0.5,rows,columns,num_frames));
+random_pattern = uint8(binornd(1,0.5,num_rows,num_columns,num_frames));
 
 %Computing coded snapshot
 summand_matrices = frames.*random_pattern;
@@ -23,9 +23,16 @@ coded_snapshot = uint8(sum(summand_matrices, 3));
 imshow(coded_snapshot);
 
 
+%%
+patch_size=8;
+block_size = patch_size^2;
+
+patch_start_row = 10;
+patch_start_column = 10;
+
 %Constructing the A matrix
-block_size = rows*columns;
 A = zeros(block_size, block_size*num_frames);
 for i=0:num_frames-1
-    A(:, (i*block_size+1):(i+1)*block_size) = diag(reshape(random_pattern(:,:,i+1),[block_size,1]));
+    A(:, (i*block_size+1):(i+1)*block_size) = diag(reshape(random_pattern(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1,i+1),[block_size,1]));
 end
+
