60ba00d1a1885db148f9b2d49bce1b9a0eaf9a11
rishi9699 <rishi9699@gmail.com>
2021-02-09 19:29:29 +0530

    Fixed reshape bug

--- testscript.m
+++ testscript.m
@@ -1,25 +1,29 @@
-num_rows=288;
-num_columns=352;
+num_rows = 120; % 288
+num_columns = 210; % 352
 num_frames=3;
 
 video=mmread('../HW1/cars.avi');
-frames = uint8(zeros(num_rows, num_columns, num_frames));
+frames = zeros(num_rows, num_columns, num_frames);
 for i=1:num_frames
-    frames(:,:,i) = im2gray(video.frames(i).cdata);
+    frames(:,:,i) = im2gray(video.frames(i).cdata(169:288, 143:352));
+    %frames(:,:,i) = im2gray(video.frames(i).cdata);
 end
 
+
 figure
-imshow(frames(:,:,1))
-figure
-imshow(frames(:,:,2))
+imshow(uint8(frames(:,:,2)))
 figure
 imshow(frames(:,:,3))
 
-random_pattern = uint8(binornd(1,0.5,num_rows,num_columns,num_frames));
+%random_pattern = binornd(1,0.7,num_rows,num_columns,num_frames);
+random_pattern = zeros(num_rows,num_columns,num_frames);
+random_pattern(:,:,2) = ones(num_rows,num_columns);
+random_pattern(:,:,1) = binornd(1,0.5,num_rows,num_columns);
+random_pattern(:,:,3) = ~random_pattern(:,:,1);
 
 % Computing coded snapshot
 coded_snapshot = sum(frames.*random_pattern, 3);
-imshow(uint16(coded_snapshot));
+imshow(uint8(coded_snapshot/3));
 
 
 %%
@@ -74,22 +78,23 @@ reconstructed_frames = zeros(num_rows, num_columns, num_frames);
 phi = zeros(block_size, block_size*num_frames);
 all_weights = zeros(num_rows, num_columns);
 
-for patch_start_row = 1:(num_rows+patch_size-1) % Iterating over all possible patches
-    for patch_start_column = 1:(num_columns+patch_size-1)
+for patch_start_row = 1:(num_rows-patch_size+1) % Iterating over all possible patches
+    for patch_start_column = 1:(num_columns-patch_size+1)
         
 
         % Constructing the phi matrix
         for i=0:num_frames-1
-            phi(:, (i*block_size+1):(i+1)*block_size) = diag(reshape(random_pattern(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1,i+1),[block_size,1]));
+            phi(:, (i*block_size+1):(i+1)*block_size) = diag(reshape(random_pattern(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1,i+1).',[1, block_size]));
         end
 
         % Implementing OMP
         indices = zeros(1, block_size);
         support_set = zeros(block_size, block_size);
-
+        
+        % Check OMP algorithm ?
         A=phi*psi;
         normalized_A = normc(A);
-        measurement = reshape(coded_snapshot(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1), [block_size,1]);
+        measurement = reshape(coded_snapshot(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1).', [1,block_size]).';
         residual = measurement;
         for iter=1:block_size
             [~, i] = max(abs(residual.'*normalized_A));
@@ -104,8 +109,9 @@ for patch_start_row = 1:(num_rows+patch_size-1) % Iterating over all possible pa
         for pos=1:block_size
             x(indices(pos)) = theta(pos);
         end
-        f = psi*x;
+        f = psi*x; % DCT forward formula ?
         f=reshape(f, [patch_size, patch_size, num_frames]); % Patch frames in the Dirac basis
+        f = permute(f, [2 1 3]);
 
         % Rejoining the patches
         weights = all_weights(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1);
@@ -116,4 +122,6 @@ for patch_start_row = 1:(num_rows+patch_size-1) % Iterating over all possible pa
         all_weights(patch_start_row:patch_start_row+patch_size-1,patch_start_column:patch_start_column+patch_size-1) ...
             = (weights+1);
     end
+    disp(patch_start_row);
 end
+% Check in-built function calls
